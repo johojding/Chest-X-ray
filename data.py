b0a7311c431dbe8cdf365d287bb77d86ae4998a2
johojding <josephine.hojding@gmail.com>
2026-07-02 20:45:01 +0200

    downloaded data and displayed image

--- data.py
+++ data.py
@@ -1,5 +1,14 @@
-import kagglehub
+import os
+from pathlib import Path
+from PIL import Image
+data_path = "/Users/josephinehojding/.cache/kagglehub/datasets/paultimothymooney/chest-xray-pneumonia/versions/2/chest_xray"
+root = Path(data_path)
+train_normal = root / "train" / "NORMAL"
+train_pneumonia = root / "train" / "PNEUMONIA"
+test = root/ "test"
+val = root/"val"
 
-path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
-
-print("Path to dataset files:", path)
+train_normal = list(train_pneumonia.rglob("*jpeg"))
+print(len(train_normal))
+img = Image.open(train_normal[0])
+img.show()
